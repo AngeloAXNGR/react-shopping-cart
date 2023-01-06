79ba11e3db39062bb38280f6b0f3ea41541acdc7
LuhxEn <axngrsantos@gmail.com>
2023-01-06 10:21:53 +0800

    Add feature to open and close cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import { Routes, Route } from "react-router-dom";
+import React from "react";
 import RouteSwitch from "./RouteSwitch";
 import Header from "./components/Header";
+import Cart from "./components/Cart";
 
 
 function App() {
+  const [openCart, setOpenCart] = React.useState(false);
+
+
+  const [cart, setCart] = React.useState([]);
+
+  const toggleCartOpen = () => {
+    setOpenCart(prevOpen => {return !prevOpen});
+  }
+
+
   return (
     <div className="app">
-      <Header/>
-      <RouteSwitch/>
+      <Header
+        showCart={toggleCartOpen}
+      />
+      <Cart 
+        open={openCart}
+        closeCart={toggleCartOpen}
+      />
+      
+      
+      <RouteSwitch
+      />
     </div>
   );
 }
