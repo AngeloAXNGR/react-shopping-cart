5fb40dea9fa15f216ef8d1e2ea39da38de2a0deb
LuhxEn <axngrsantos@gmail.com>
2023-01-06 10:23:58 +0800

    Add cart Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
-const Header = () => {
+import CartIcon from '../images/cart.svg'
+const Header = ({showCart}) => {
+
+
   return(
     <header>
       <h1 id="header-title">eCommerce Site</h1>
       <div className="header-links">
         <NavLink to="/">Home</NavLink>
         <NavLink to="products">Products</NavLink>
+        <img onClick={showCart} id="cart-icon" src={CartIcon} alt="" />
       </div>
     </header>
   )
