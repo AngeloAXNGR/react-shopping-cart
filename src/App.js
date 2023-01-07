2f4d31bf42bb1b78ac4d45bbc8e5ee9b74963212
LuhxEn <axngrsantos@gmail.com>
2023-01-07 09:33:32 +0800

    Add function to add and  subtract quantatity along with computing running total

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Routes, Route } from "react-router-dom";
 import React from "react";
 import RouteSwitch from "./RouteSwitch";
 import Header from "./components/Header";
@@ -9,7 +8,20 @@ function App() {
   const [openCart, setOpenCart] = React.useState(false);
 
 
-  const [cart, setCart] = React.useState([]);
+  const [cart, setCart] = React.useState(() => {return JSON.parse(localStorage.getItem('cart')) || []});
+  const [runningTotal, setRunningTotal] = React.useState(0.00);
+
+  React.useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+    
+
+    let total = 0;
+    for(let i = 0; i < cart.length; i++){
+      total += parseFloat(cart[i].quantity * cart[i].price)
+    }
+    setRunningTotal(total.toFixed(2));
+  },[cart])
+
 
   const toggleCartOpen = () => {
     setOpenCart(prevOpen => {return !prevOpen});
@@ -33,6 +45,32 @@ function App() {
 
   }
 
+  const addQuantity = (id) =>{
+    setCart(prevCart => {
+      return prevCart.map(item =>{
+        return item.id === id ? {...item, quantity: item.quantity + 1} : item;
+      })
+    })
+  }
+
+  const subtractQuantity = (id) =>{
+    const selectedCartItem = cart.find(item => item.id === id);
+
+    // Check if quantity of current item is less than 2
+    // If it is, remove cart item after clicking subtract button again 
+    if(selectedCartItem.quantity < 2){
+      setCart(prevCart => {
+        return prevCart.filter(item => item.id !== id);
+      })
+    }else{
+      setCart(prevCart => {
+        return prevCart.map(item =>{
+          return item.id === id ? {...item, quantity: item.quantity - 1} : item;
+        })
+      })
+    }
+  }
+
   console.log(cart)
 
   return (
@@ -41,8 +79,12 @@ function App() {
         open={openCart}
         closeCart={toggleCartOpen}
         cart={cart}
+        addQuantity={addQuantity}
+        subtractQuantity={subtractQuantity}
+        runningTotal={runningTotal}
       />
       <Header
+        itemCount={cart.length}
         showCart={toggleCartOpen}
       />
 
