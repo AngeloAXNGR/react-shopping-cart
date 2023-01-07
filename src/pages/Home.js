da708a393e480916568dccbaabddeff89c1ce098
LuhxEn <axngrsantos@gmail.com>
2023-01-07 18:31:10 +0800

    Finish homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
+import HeroImage from '../images/hero-image.jpg'
+import { NavLink } from "react-router-dom";
+
 const Home = () =>{
   return(
     <div className="homepage">
-      Home Page
+      <div className="hero-desc">
+        <h1>RifeCart</h1>
+        <p>Your One-Stop shop for Fashion, Jewelry, and Tech.</p>
+        <NavLink to="products">Products</NavLink>
+      </div>
+      <div className="hero-image">
+        <img src={HeroImage} alt="" />
+      </div>
     </div>
   )
 }
